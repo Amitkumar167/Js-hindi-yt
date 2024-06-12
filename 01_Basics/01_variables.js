859ff16824587348ee307a3a644b941674877990
Amit Kumar <118620325+Amitkumar167@users.noreply.github.com>
2024-06-12 09:30:14 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 2003636
+let account_email = "amit@google.com"
+var account_Password = "112233"
+accountcity = "Jammu"
+
+// const changes not allowed
+// variable can be changed
+// avoid the use of var 
+// prefer the use of let only for variables 
+
+console.table([accountId, account_email, account_Password, accountcity]);
