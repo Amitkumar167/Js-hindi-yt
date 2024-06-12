492e608a6ad394cef4f3a61a2c21e2bec9a7d979
Amit Kumar <118620325+Amitkumar167@users.noreply.github.com>
2024-06-12 10:32:13 +0000

    container config done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,15 +2,22 @@ const accountId = 2003636
 let account_email = "amit@google.com"
 var account_Password = "112233"
 accountcity = "Jammu"
+let accountState;
 
 // const changes not allowed
 // variable can be changed
+account_email = "amit@gmail.com"
+
 /* avoid the use of var
  because of block scope problem and functional scope
 */
-
+/*
+ semicolons are optional 
+*/ 
 /* 
 prefer the use of let only for variables 
 */
 
-console.table([accountId, account_email, account_Password, accountcity]);
+
+
+console.table([accountId, account_email, account_Password, accountcity, accountState]);
