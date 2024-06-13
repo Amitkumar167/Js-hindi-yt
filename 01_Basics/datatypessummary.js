7cf182fd4b8c9bc4acd93dd86d32e29587f14257
Amit Kumar <118620325+Amitkumar167@users.noreply.github.com>
2024-06-13 08:03:04 +0000

    datatypessummary

--- 01_Basics/datatypessummary.js
+++ 01_Basics/datatypessummary.js
@@ -2,36 +2,38 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+ const score = 100
+ const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+const bigNumber = 3456543576654356754n
 
+// console.log(bigNumber);
+// console.log(typeof bignumber);
 
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+//  https://262.ecma-international.org/5.1/#sec-11.4.3
